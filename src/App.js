b5a4e64dee92541b86e44b38aba423ba344bba9a
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-13 20:55:02 +0100

    example

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ import './App.css';
 class App extends PureComponent {
   state = {
     isUserLogged: defaultObject.isUserLogged,
+    isUserAdult: true,
   }
 
   handleToggleStateIsLogged = () => this.setState(prevState => ({
-    isUserLogged: !prevState.isUserLogged
+    isUserLogged: !prevState.isUserLogged,
+  }));
+
+  handleToggleStateIsAdult = () => this.setState(prevState => ({
+    isUserAdult: !prevState.isUserAdult,
   }));
 
   render() {
@@ -22,6 +27,13 @@ class App extends PureComponent {
         <AppContext.Provider value={{
           isUserLogged: this.state.isUserLogged,
           toggleLoggedState: this.handleToggleStateIsLogged
+        }}>
+          <UserInfo />
+          <Button />
+        </AppContext.Provider>
+        <AppContext.Provider value={{
+          isUserLogged: this.state.isUserAdult,
+          toggleLoggedState: this.handleToggleStateIsAdult
         }}>
           <UserInfo />
           <Button />
