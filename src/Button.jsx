853bda3f42056b4b2a5998bc21f40463ae359803
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-13 20:59:17 +0100

    example 3

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,19 +2,15 @@ import React, { PureComponent } from "react";
 import { AppContext } from "./AppContent";
 
 class Button extends PureComponent {
+  static contextType = AppContext;
+
   render() {
     return (
-      <AppContext.Consumer>
-        {
-          ({ toggleLoggedState }) => (
-            <button
-              onClick={toggleLoggedState}
-            >
-              Przełącz stan użytkownika
-            </button>
-          )
-        }
-      </AppContext.Consumer>
+      <button
+        onClick={this.context.toggleLoggedState}
+      >
+        Przełącz stan użytkownika
+      </button>
     );
   }
 }
