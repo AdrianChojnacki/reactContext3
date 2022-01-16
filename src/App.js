eece86f80654c586b82aecbaef1af6a151c5733a
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-16 14:58:28 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -9,18 +9,12 @@ import './App.css';
 
 const App = () => {
   const [isUserLogged, setIsUserLogged] = useState(defaultObject.isUserLogged);
-  const [isUserAdult, setIsUserAdult] = useState(true);
 
   const toggleLoggedState = () => setIsUserLogged(prevValue => !prevValue);
-  const toggleAdultState = () => setIsUserAdult(prevValue => !prevValue);
 
   return (
     <div>
       <AppContext.Provider value={{ isUserLogged, toggleLoggedState }}>
-        <UserInfo />
-        <Button />
-      </AppContext.Provider>
-      <AppContext.Provider value={{ isUserAdult, toggleAdultState }}>
         <UserInfo />
         <Button />
       </AppContext.Provider>
--- src/AppContent.js
+++ src/AppContent.js
@@ -3,8 +3,6 @@ import { createContext } from 'react';
 export const defaultObject = {
   isUserLogged: false,
   toggleLoggedState: () => console.log('Domyślny Provider'),
-  isUserAdult: false,
-  toggleAdultState: () => console.log('Domyślny Provider'),
 }
 
 export const AppContext = createContext(defaultObject);
